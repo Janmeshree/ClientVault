438bbd8eaac10601a8baab27248f6019467cebdf
Janmeshree <janmeshree2005@gmail.com>
2026-05-09 14:52:24 +0530

    Delete src directory

--- src/main/java/com/company/clientsystem/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.clientsystem.repository;
-
-import com.company.clientsystem.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
